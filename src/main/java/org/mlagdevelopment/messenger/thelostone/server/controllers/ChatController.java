224f84fee710ea21938252fe493fefb79febf651
MLag <disw123321@gmail.com>
2026-09-13 23:17:51 +0700

    chat and room is working

--- src/main/java/org/mlagdevelopment/messenger/thelostone/server/controllers/ChatController.java
+++ src/main/java/org/mlagdevelopment/messenger/thelostone/server/controllers/ChatController.java
@@ -1,12 +1,28 @@
 package org.mlagdevelopment.messenger.thelostone.server.controllers;
 
+import org.mlagdevelopment.messenger.thelostone.server.dto.SendMessageRequest;
+import org.mlagdevelopment.messenger.thelostone.server.service.MessageService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 public class ChatController {
 
-    //private final MessageService;
+    private final MessageService messageService;
+
+    public ChatController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @MessageMapping("/chat.send")
+    public void send(@Payload SendMessageRequest request, Principal principal) {
+        Long userId = Long.parseLong(principal.getName());
+        messageService.sendMessage(userId, request);
+    }
 
 
 }
--- src/test/java/org/mlagdevelopment/messenger/thelostone/StompTest.java
+++ src/test/java/org/mlagdevelopment/messenger/thelostone/StompTest.java
@@ -1,22 +1,33 @@
 package org.mlagdevelopment.messenger.thelostone;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.jspecify.annotations.Nullable;
+import org.mlagdevelopment.messenger.thelostone.server.dto.ChatMessage;
+import org.mlagdevelopment.messenger.thelostone.server.dto.SendMessageRequest;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.simp.stomp.*;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 
+import java.lang.reflect.Type;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class StompTest {
     public static void main(String[] args) throws Exception {
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4IiwidXNlcm5hbWUiOiJURVNUTUxBRyIsImlhdCI6MTc4OTI4NzgyOSwiZXhwIjoxNzg5Mzc0MjI5fQ.nzz4HxzvNk6d2sZzJboofqDJpwbQmVBwbv9Lbt7wfjI";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4IiwidXNlcm5hbWUiOiJURVNUTUxBRyIsImlhdCI6MTc4OTMxNTA1NiwiZXhwIjoxNzg5NDAxNDU2fQ.jnURq8o2hRjgnYb3RbjG9njK9wGdGCAs6r5SMP_if5A";
 
         WebSocketStompClient client = new WebSocketStompClient(new StandardWebSocketClient());
         client.setMessageConverter(new MappingJackson2MessageConverter());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
 
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(mapper);
+
+        client.setMessageConverter(converter);
         WebSocketHttpHeaders handshakeHeaders = new WebSocketHttpHeaders();
         StompHeaders connectHeaders = new StompHeaders();
         connectHeaders.add("Authorization", "Bearer " + token);
@@ -44,8 +55,28 @@ public class StompTest {
             System.out.println("Connected: " + session.isConnected());
             System.out.println("Session ID: " + session.getSessionId());
 
-            session.disconnect();
-            System.out.println("Disconnected");
+            session.subscribe("/topic/room.4", new StompFrameHandler() {
+
+                @Override
+                public Type getPayloadType(StompHeaders headers) {
+                    return ChatMessage.class;
+                }
+
+                @Override
+                public void handleFrame(StompHeaders headers, @Nullable Object payload) {
+                    ChatMessage msg = (ChatMessage) payload;
+                    System.out.println("<< ["+msg.senderUiName()+"]: "+msg.content());
+                }
+            });
+
+
+            while (session.isConnected()){
+                Scanner scanner = new Scanner(System.in);
+                String word = scanner.nextLine();
+                session.send("/app/chat.send",new SendMessageRequest(4L,word));
+            }
+            //  session.disconnect();
+            //System.out.println("Disconnected");
 
         } catch (Exception e) {
             System.out.println("Failed: " + e.getMessage());
